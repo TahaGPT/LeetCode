5e4779d61b19f81629f4d8e0fb3c6ef447f2360b
CodesbyTaha <tahaaahmed12115@gmail.com>
2024-08-22 02:42:44 +0500

    Two Sum Problem Solved

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -10,16 +10,16 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        for (int i = 0; i < nums.size() - 1; i++)
+        for (int i = 0; i < nums.size() - 1; i++) // skipping the last element to avoid the overflow in the next loop
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            for (int j = i + 1; j < nums.size(); j++) // starting from the nexxt element to avoid ctradictions
             {
-                if (nums[i] + nums[j] == target)
+                if (nums[i] + nums[j] == target) // if the sum becomes equal it'll return the indices
                 {
                     return {i, j};
                 }
             }
         }
-        return {-1, -1};
+        return {-1, -1}; // if we don't find any solution, both the indices will be zero
     }
 };
